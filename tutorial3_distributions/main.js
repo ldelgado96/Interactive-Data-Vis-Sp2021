39de0583a7cbc452016c9c081c8f5ed16f5da593
ldelgado96 <lilybeth.delgado96@gmail.com>
2021-03-01 01:46:09 -0600

    adding all changes to my files

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -9,66 +9,157 @@ const width = window.innerWidth * 0.7,
 let svg;
 let xScale;
 let yScale;
+let colorScale;
 
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selection: "All" // + YOUR FILTER SELECTION
+  selection:'Monday' // + YOUR FILTER SELECTION
 };
 
 /* LOAD DATA */
-d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
+/**d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
   // + SET YOUR DATA PATH
   console.log("raw_data", raw_data);
   state.data = raw_data;
   init();
+}); **/
+
+d3.json("../data/sleepdata.json",d3.autoType).then(raw_data =>{
+
+  console.log("raw_data",raw_data);
+  state.data = raw_data;
+  init();
 });
+
 
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in 
 function init() {
-  // + SCALES
+ // + SCALES
 
-  // + AXES
+  xScale = d3.scaleLinear()
+    .domain(d3.extent(state.data, d=> d.minutes_asleep))
+    .range([margin.left, width - margin.right])
+    
+
+    yScale = d3.scaleLinear()
+    .domain(d3.extent(state.data, d => d.number_of_awakenings))
+    .range([height - margin.bottom, margin.bottom])
+
+  // + AXES 
+  const x = d3.axisBottom(xScale) 
+  const y = d3.axisLeft(yScale)
+
+
+
+  //Create the svg
+
+  svg = d3.select("#d3-container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height",height)
 
   // + UI ELEMENT SETUP
+   // add labels - xAxis
+    // + CALL AXES
+  const xAxisGroup = svg.append("g")
+  .attr("class", 'xAxis')
+  .attr("transform", `translate(${0}, ${height - margin.bottom})`) // move to the bottom
+  .call(x)
 
-  const selectElement = d3.select("#dropdown").on("change", function() {
-    // `this` === the selectElement
-    // 'this.value' holds the dropdown value a user just selected
+const yAxisGroup = svg.append("g")
+  .attr("class", 'yAxis')
+  .attr("transform", `translate(${margin.left}, ${0})`) // align with left margin
+  .call(y)
 
-    state.selection = this.value
-    console.log("new value is", this.value);
-    draw(); // re-draw the graph based on this new selection
-  });
+// add labels - xAxis
+xAxisGroup.append("text")
+  .attr("class", 'axis-title')
+  .attr("x", width / 2)
+  .attr("y", 40)
+  .attr("text-anchor", "middle")
+  .style("fill","#FFFF00")
+  .style("font", "15px courier,arial,helvetica")
+  .text("Minutes Sleeping")
 
-  // add in dropdown options from the unique values in the data
-  selectElement
-    .selectAll("option")
-    .data(["All", "1", "2", "3"]) // + ADD UNIQUE VALUES
+// add labels - yAxis
+yAxisGroup.append("text")
+  .attr("class", 'axis-title')
+  .attr("x", -40)
+  .attr("y", height / 2)
+  .attr("writing-mode", "vertical-rl")
+  .attr("text-anchor", "middle")
+  .style("fill","#FFFF00")
+  .style("font", "15px courier,arial,helvetica")
+  .text("Amount of times I woke up during my sleep")
+
+   // append a new group, g stands for group
+   svg.append("g")
+   .attr("class", "xAxis")
+   .attr("transform", `translate(${0},${height-margin.bottom})`)
+   .call(x)
+
+   svg.append("g")
+   .attr("class", "yAxis")
+   .attr("transform", `translate(${margin.left},${0})`)
+   .call(y)
+
+
+  // Setting up the UI Elements
+  const listoptions =  d3.select("#dropdown")
+
+    listoptions.selectAll("options")
+    .data(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"])
     .join("option")
-    .attr("value", d => d)
-    .text(d => d);
+    .attr("value", d=>d)
+    .text (d=>d)
 
-  // + CREATE SVG ELEMENT
+  listoptions.on("change", event => {
 
-  // + CALL AXES
+    state.selection = event.target.value
+    draw();
 
-  draw(); // calls the draw function
+  })
+    // calls the draw function
+   draw(); 
+
 }
 
 /* DRAW FUNCTION */
  // we call this everytime there is an update to the data/state
 function draw() {
-  
-  // + FILTER DATA BASED ON STATE
 
-  // const dot = svg
-  //   .selectAll("circle")
-  //   .data(filteredData, d => d.name)
-  //   .join(
-  //     enter => enter, // + HANDLE ENTER SELECTION
-  //     update => update, // + HANDLE UPDATE SELECTION
-  //     exit => exit // + HANDLE EXIT SELECTION
-  //   );
+  const userInput= state.data.filter(d=>d.day === state.selection)
+
+  svg.selectAll("circle")
+  .data(userInput, d=>d.id)
+
+  .join(
+     enter => enter.append("circle")
+     .attr("fill", '#FFFF00')
+     .attr("r", radius*1.5)
+     
+     .call(enter=> enter.transition()
+     .ease(d3.easeLinear)
+      .duration(850)
+      .attr("cy", d=> yScale(d.number_of_awakenings))
+    )
+    ,
+    update => update,
+    
+     exit => exit
+     .call(sel => sel
+       .attr("opacity", 1)
+       .transition()
+       .duration(500)
+       .attr("opacity", "0")
+       .remove()
+     )
+  )
+     
+  
+  /* .attr("cy", d=> yScale(d.minutes_awake))*/
+  .attr("cx", d=> xScale(d.minutes_asleep))
+ 
 }
